3b32929625f5bdbe68d401b8e3cf1a912aa0508c
Julien Dubois <julien.dubois@gmail.com>
2021-03-26 13:52:00 +0100

    first commit

---
+++ src/main/java/com/example/the425show/The425showApplication.java
@@ -0,0 +1,13 @@
+package com.example.the425show;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class The425showApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(The425showApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/the425show/The425showApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.the425show;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class The425showApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
