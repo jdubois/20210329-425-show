d5b671be2244626760d090c5e3fb0633bca57be6
Julien Dubois <julien.dubois@gmail.com>
2021-03-29 16:18:50 +0200

    Add method security

--- src/main/java/com/example/the425show/SecurityController.java
+++ src/main/java/com/example/the425show/SecurityController.java
@@ -1,7 +1,7 @@
 package com.example.the425show;
 
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SecurityController {
 
     @GetMapping("/")
+    @Secured("ROLE_admin")
     public String hello() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return "Hello, " + authentication.getName()  +
