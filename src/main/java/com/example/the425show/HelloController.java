1792f307dc03b1a75b2063bc079997a234302750
Julien Dubois <julien.dubois@gmail.com>
2021-03-30 11:21:34 +0200

    dump headers

--- src/main/java/com/example/the425show/HelloController.java
+++ src/main/java/com/example/the425show/HelloController.java
@@ -1,27 +1,41 @@
 package com.example.the425show;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.cloud.CloudPlatform;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.Iterator;
 
 @RestController
 public class HelloController {
 
     private final Environment environment;
 
-
-
     public HelloController(Environment environment) {
         this.environment = environment;
     }
 
-    @GetMapping("/")
-    public String hello() {
 
+
+    @GetMapping("/")
+    public String hello(HttpServletRequest request) {
         return
-                "env=" + environment.getProperty("server.forward-headers-strategy") + "\n" +
-                        "cloud=" + CloudPlatform.getActive(environment) + "\n";
+                "env=" +
+                        environment.getProperty("server.forward-headers-strategy") + "\n" +
+                        "cloud=" + CloudPlatform.getActive(environment) + "\n" +
+                "headers=" + printHeaders(request);
+    }
+
+    private String printHeaders(HttpServletRequest request) {
+        String headers = "";
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while(headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            headers += "Header Name - " + headerName + ", Value - " + request.getHeader(headerName) + " | ";
+        }
+        return headers;
     }
 }
